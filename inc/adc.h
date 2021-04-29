4c03cd42697e78425fff71afa683286f92806124
jagadeesharadhyula7608 <jagadeesharadhyula7608@gmail.com>
2021-04-29 21:48:37 +0530

    macros

--- inc/adc.h
+++ inc/adc.h
@@ -2,6 +2,10 @@
 #define ADC_H_INCLUDED
 
 #include "gpio.h"
+#define CHSEL (7<<MUX0)
+#define ADC_CONV_START (1<<ADSC)
+#define  ADC_CONV_COMPLETE (1<<ADIF)
+#define ADC_ENABLE (1<<ADEN)|(7<<ADPS0)
 
 void adc(act_out * ptr);
 uint16_t readAdc(uint8_t ch);
--- src/adc.c
+++ src/adc.c
@@ -2,12 +2,12 @@
 #include "adc.h"
 uint16_t readAdc(uint8_t ch)
 {
-ADMUX&=~(7<<MUX0);
+ADMUX&=~CHSEL;
 ch&=0xff;// GET THE CHANNEL NUMBER
 ADMUX|=ch;//SELECT THE CHANNEL USING MUX REG
-ADCSRA|=(1<<ADSC);// START THE ADC CONVERSION
+ADCSRA|=ADC_CONV_START;// START THE ADC CONVERSION
 while(!(ADCSRA&(1<<ADIF)));//WAIT TILL THE CONVERSION COMPLETES
-ADCSRA|=(1<<ADIF);
+ADCSRA|=ADC_CONV_COMPLETE;// ADC CONVERSION COMPLETES
  return(ADC);
 }
 
@@ -16,12 +16,12 @@ void adc(act_out * ptr)
 if(ptr->state)
 {
     ADMUX|=(1<<REFS0);
-    ADCSRA|=(1<<ADEN)|(7<<ADPS0);
+    ADCSRA|=ADC_ENABLE;
     ptr->adcvalue=readAdc(0);
 }
 else
 {
-    ADCSRA&=~(1<<ADEN);
+    ADCSRA&=~ADC_ENABLE;
     ADC=0;
 }
 }
